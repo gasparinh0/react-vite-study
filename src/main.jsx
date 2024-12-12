3be35d4e92f6e2fa3e639506b465c5f228be69d9
Lucas Gaspari <lcsgaspari@gmail.com>
2024-12-11 21:35:01 -0300

    adding a color theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
 import App from './App.jsx'
+import './main.css'
+
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { pink, purple } from '@mui/material/colors';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[700],
+    },
+    secondary: {
+      main: pink[900],
+    },
+  },
+});
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
