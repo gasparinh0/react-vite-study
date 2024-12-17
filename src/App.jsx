225ddf8e3453fb3a0578c8db9dd9ba2da7112673
Lucas Gaspari <lcsgaspari@gmail.com>
2024-12-17 11:40:20 -0300

    adding routes to CustomersRegister

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router'
 
 //Componentes
 import Home from './pages/Home.jsx'
-import Customers from './pages/Customers.jsx'
+import CustomersList from './pages/customers/List.jsx'
+import CustomersRegister from './pages/customers/Register.jsx'
 
 import TemplateDefault from "./templates/Default.jsx"
 import TemplatePage from "./templates/Page.jsx"
+
+import { ToastContainer } from 'react-toastify'; //Imports do react-toastify
 
 function App() {
 
   return (
     <Router>
       <TemplateDefault>
+      <ToastContainer />
         <Routes>
+          <Route path="/customers/add" element={<TemplatePage title="Cadastro de clientes" Component={CustomersRegister} />} />
+          <Route path="/customers" element={<TemplatePage title="Lista de clientes" Component={CustomersList} />} />
           <Route path="/" element={<TemplatePage title="Home" Component={Home} />} />
-          <Route path="/customers" element={<TemplatePage title="Customers" Component={Customers} />} />
         </Routes>
       </TemplateDefault>
     </Router>
