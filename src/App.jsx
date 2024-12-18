d713e0c34f7fe4d74a42246f6125cf6add15ddc9
Lucas Gaspari <lcsgaspari@gmail.com>
2024-12-17 21:31:51 -0300

    adding route to CustomersEdit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import Home from './pages/Home.jsx'
 import CustomersList from './pages/customers/List.jsx'
 import CustomersRegister from './pages/customers/Register.jsx'
+import CustomersEdit from './pages/customers/Edit.jsx'
 
 import TemplateDefault from "./templates/Default.jsx"
 import TemplatePage from "./templates/Page.jsx"
@@ -17,6 +18,7 @@ function App() {
       <TemplateDefault>
       <ToastContainer />
         <Routes>
+          <Route path="/customers/edit/:id" element={<TemplatePage title="Editar cliente" Component={CustomersEdit} />} />
           <Route path="/customers/add" element={<TemplatePage title="Cadastro de clientes" Component={CustomersRegister} />} />
           <Route path="/customers" element={<TemplatePage title="Lista de clientes" Component={CustomersList} />} />
           <Route path="/" element={<TemplatePage title="Home" Component={Home} />} />
