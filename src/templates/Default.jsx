5b69e36abf3d6675cbeca92b005d8a139b40a698
Lucas Gaspari <lcsgaspari@gmail.com>
2024-12-19 20:16:01 -0300

    adding user from useAuth

--- src/templates/Default.jsx
+++ src/templates/Default.jsx
@@ -1,11 +1,14 @@
 import Container from '@mui/material/Container';
 
 import Header from "../partials/Header.jsx"
+import useAuth from "../state/auth.jsx"
 
 export default function Template({ children }) {
+    const { user } = useAuth()
+
     return (
         <>
-            <Header />
+            <Header user={user}/>
             <Container sx = {{ padding: '15px 0' }}>
                 {children}
             </Container>
