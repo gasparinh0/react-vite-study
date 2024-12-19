a8b4345128dbe72816ae5d96475e9e3ada82c4ce
Lucas Gaspari <lcsgaspari@gmail.com>
2024-12-19 20:15:35 -0300

    creating new template only for login

---
+++ src/templates/Clean.jsx
@@ -0,0 +1,11 @@
+import Container from '@mui/material/Container';
+
+export default function Clean({ Component }) {
+    return (
+        <>
+            <Container sx = {{ padding: '15px 0' }}>
+                <Component />
+            </Container>
+        </>
+    )
+}
